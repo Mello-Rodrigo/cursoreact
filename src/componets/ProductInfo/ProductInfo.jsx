493a361c1d501f8a50be6a22d115a5743b7bfc77
mello-rodrigo <mellorodrigo718@gmail.com>
2023-05-03 21:54:50 -0300

    entrega

---
+++ src/componets/ProductInfo/ProductInfo.jsx
@@ -0,0 +1,13 @@
+import './ProductInfo.css'
+
+const ProductInfo = () => {
+  return (
+    <div className='product-info'>
+       <h1>Producto</h1>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, incidunt delectus accusantium voluptatibus eos explicabo ratione! Dolorum libero nostrum eligendi. Veritatis labore iste quas adipisci incidunt velit quia odio expedita?</p>
+       <p>$100</p>
+    </div>
+  )
+}
+
+export default ProductInfo
